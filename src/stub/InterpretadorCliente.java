26eba1194fb5933a647c818f1c472833744ef8fd
Marcelo Torres <marcelo-torres>
2019-10-02 00:21:25 -0300

    Criacao da interface Destinatario e adequacao do codigo

--- src/stub/InterpretadorCliente.java
+++ src/stub/InterpretadorCliente.java
@@ -2,15 +2,48 @@ package stub;
 
 import Logger.Logger;
 import static Logger.Logger.Tipo.ERRO;
-import java.io.Closeable;
+import static Logger.Logger.Tipo.INFO;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.LinkedList;
 import stub.comunicacao.Comunicador;
-import stub.comunicacao.Mensageiro;
+import stub.comunicacao.Destinatario;
 
-public class InterpretadorCliente implements Closeable {
- 
-    private final Mensageiro MENSAGEIRO;
+/**
+ * blablablala escrever
+ */
+public class InterpretadorCliente extends Destinatario {
+
+    /**
+     * Gerenciador de exceptions nao capturadas por metodos, isto eh, que
+     * ocorreram em outras threads.
+     */
+    public static class GerenciadorDeException implements Thread.UncaughtExceptionHandler {
+    
+        private final LinkedList<Destinatario> DESTINATARIOS = new LinkedList();
+        
+        public void setGerenciadorDeException(Destinatario destinatario) {
+            this.DESTINATARIOS.add(destinatario);
+        }
+        
+        @Override
+        public void uncaughtException(Thread th, Throwable ex) {
+            Logger.registrar(ERRO, new String[]{"INTERPRETADOR"}, "Erro na comunicacao: " + ex.getMessage());
+            Logger.registrar(INFO, new String[]{"INTERPRETADOR"}, "Encerrando devido a falha de comunicacao");
+            
+            for(Destinatario destinatario : this.DESTINATARIOS) {
+                try {
+                    destinatario.close();
+                } catch(IOException ioe) {
+                    Logger.registrar(ERRO, new String[]{"INTERPRETADOR"}, "Erro na comunicacao: " + ioe.getMessage());
+                    Logger.registrar(INFO, new String[]{"INTERPRETADOR"}, "Encerrando devido a falha de comunicacao");
+                }
+            }
+        }
+        
+    }
+    
+    private final static GerenciadorDeException GERENCIADOR_DE_EXCEPTION = new GerenciadorDeException();
     
     public InterpretadorCliente(
             int portaEscutarUDP,
@@ -18,29 +51,32 @@ public class InterpretadorCliente implements Closeable {
             int portaTCPDoServidor,
             int portaUDPDoServidor) {
         
-        this.MENSAGEIRO = new Mensageiro(
-                this,
+        super(
                 Comunicador.Modo.CLIENTE,
                 portaEscutarUDP,
                 enderecoDoServidor,
                 portaTCPDoServidor,
-                portaUDPDoServidor);
+                portaUDPDoServidor,
+                GERENCIADOR_DE_EXCEPTION);
+        GERENCIADOR_DE_EXCEPTION.setGerenciadorDeException(this);
     }
     
     public void iniciar() throws Exception {
         try {
-            this.MENSAGEIRO.iniciarTCP();
+            super.MENSAGEIRO.iniciarTCP();
         } catch(IOException ioe) {
             Logger.registrar(ERRO, new String[]{"INTERPRETADOR"}, "Erro ao tentar iniciar a comunicacao.");
             throw new Exception("Nao foi possivel iniciar a comunicacao com o servidor");
         }
     }
+   
     
     @Override
     public void close() {
-        this.MENSAGEIRO.close();
+        super.MENSAGEIRO.close();
     }
     
+    @Override
     public void receberMensagem(byte[] mensagem) {
         if(mensagem == null) {
             System.out.println("[!] Mano, vc ta jogando uma mensagem nula no interpretador! O que vc tem na cabeça tiw? Programa direito zeh mane");
@@ -48,17 +84,18 @@ public class InterpretadorCliente implements Closeable {
         System.out.println("[Interpretador] Mensagem recebida: " + new String(mensagem));
     }
     
+    
     public void enviarMensagemTCPLembrarDeApagarEsteMetodo(byte[] mensagem) {
-        this.MENSAGEIRO.inserirFilaEnvioTCP(mensagem);
+        super.MENSAGEIRO.inserirFilaEnvioTCP(mensagem);
     }
     
     public void enviarMensagemUDPLembrarDeApagarEsteMetodo(byte[] mensagem) {
-        this.MENSAGEIRO.inserirFilaEnvioUDP(mensagem);
+        super.MENSAGEIRO.inserirFilaEnvioUDP(mensagem);
     }
     
     public void algumMetodoQueVaiPrecisarUsarConexaoUDP() {
         try {
-            this.MENSAGEIRO.iniciarUDP();
+            super.MENSAGEIRO.iniciarUDP();
         } catch(IOException ioe) {
             // Transparencia total eh impossivel
             throw new RuntimeException("Nao foi possivel executar o metodo algumMetodoQueVaiPrecisarUsarConexaoUDP");
